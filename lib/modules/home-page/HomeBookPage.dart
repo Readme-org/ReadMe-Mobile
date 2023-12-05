e618a4cf15a42fe150e4f79de2efc2dccb27eb00
GhanyR <neoperkasa@gmail.com>
2023-12-05 14:28:23 +0700

    nambahin untuk search

--- lib/modules/home-page/HomeBookPage.dart
+++ lib/modules/home-page/HomeBookPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:readme/modules/list-book/list.dart';
+// import 'package:readme/modules/list-book/list.dart';
 import 'package:readme/widgets/appbar.dart';
 import 'package:readme/widgets/navbar.dart';
 import 'package:readme/widgets/background.dart';
