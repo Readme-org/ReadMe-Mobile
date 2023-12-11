1ab2467ede018bada407ef6761272e3680d026de
GhanyR <neoperkasa@gmail.com>
2023-12-11 15:35:12 +0700

    pull

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:readme/authentication/user.dart';
@@ -6,134 +7,173 @@ import 'package:readme/authentication/register.dart';
 import 'package:readme/modules/home-page/HomeBookPage.dart';
 
 class LoginApp extends StatelessWidget {
-const LoginApp({super.key});
+  const LoginApp({super.key});
 
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Login',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-    ),
-    home: const LoginPage(),
+      title: 'Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const LoginPage(),
     );
-    }
+  }
 }
 
 class LoginPage extends StatefulWidget {
-    const LoginPage({super.key});
+  const LoginPage({super.key});
 
-    @override
-    _LoginPageState createState() => _LoginPageState();
+  @override
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-    final TextEditingController _usernameController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
-    @override
-    Widget build(BuildContext context) {
-        final request = context.watch<CookieRequest>();
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text('Login'),
-            ),
-            body: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                          controller: _usernameController,
-                          decoration: const InputDecoration(
-                              labelText: 'Username',
-                          ),
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+    _animation = Tween<double>(begin: 0.0, end: 15.0).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 1.5; // Slow down animations for better UX
+    final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedBuilder(
+                  animation: _animation,
+                  builder: (context, child) {
+                    return Container(
+                      padding: EdgeInsets.all(_animation.value),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue.shade400.withOpacity(0.5),
                       ),
-                      const SizedBox(height: 12.0),
-                      TextField(
-                          controller: _passwordController,
-                          decoration: const InputDecoration(
-                              labelText: 'Password',
-                          ),
-                          obscureText: true,
-                      ),
-                      const SizedBox(height: 24.0),
-                      ElevatedButton(
-                          onPressed: () async {
-                              String username = _usernameController.text;
-                              String password = _passwordController.text;
+                      child: child,
+                    );
+                  },
+                  child: FlutterLogo(size: 100),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 12.0),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
 
-                              // final response = await request.login("https://readme-c11-tk.pbp.cs.ui.ac.id/auth/login/", {
+                    final response = await request.login(
+                      "http://127.0.0.1:8000/auth/login/",
+                      {'username': username, 'password': password},
+                    );
 
-                              // For testing * Uncomment ketika ingin uji coba melalui runserver :), tapi di comment line atas
-                              final response = await request.login("http://127.0.0.1:8000/auth/login/", {
-                              
-                              'username': username,
-                              'password': password,
-                              });
-                  
-                              if (request.loggedIn) {                                    
-                                  String message = response['message'];
-                                  String uname = response['username'];
+                    if (request.loggedIn) {
+                      String message = response['message'];
+                      String uname = response['username'];
+                      UserData biguname = UserData(isLoggedIn: true, username: uname);
 
-                                  biguname = UserData(isLoggedIn: true, username: uname);
-
-                                  Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => HomebookPage()),
-                                  );
-                                  ScaffoldMessenger.of(context)
-                                      ..hideCurrentSnackBar()
-                                      ..showSnackBar(
-                                          SnackBar(content: Text("$message Selamat datang, $uname.")));
-                                  } else {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                          title: const Text('Login Gagal'),
-                                          content:
-                                              Text(response['message']),
-                                          actions: [
-                                              TextButton(
-                                                  child: const Text('OK'),
-                                                  onPressed: () {
-                                                      Navigator.pop(context);
-                                                  },
-                                              ),
-                                          ],
-                                      ),
-                                  );
-                              }
-                          },
-                          child: const Text('Login'),
-                      ),
-                      // Tombol untuk register
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Don't have an account? "),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context) => RegisterPage()), 
-                                );
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomebookPage()),
+                      );
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(SnackBar(content: Text("$message Selamat datang, $uname.")));
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Login Gagal'),
+                          content: Text(response['message']),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
                               },
-                              child: Text(
-                                'Register here',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
                             ),
                           ],
                         ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue.shade900,
+                  ),
+                  child: const Text('Login'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account? "),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => RegisterPage()),
+                          );
+                        },
+                        child: Text(
+                          'Register here',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
                       ),
                     ],
+                  ),
                 ),
+              ],
             ),
-        );
-    }
+          ),
+        ),
+      ),
+    );
+  }
 }
