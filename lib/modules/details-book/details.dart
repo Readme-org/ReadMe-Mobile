ccdfae1d3743043f93efd6ac888986c160415ba6
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2023-12-18 00:23:21 +0700

    minor design changes

--- lib/modules/details-book/details.dart
+++ lib/modules/details-book/details.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:readme/modules/diskusi-book/diskusiBook.dart';
+import 'package:readme/modules/diskusi-book/diskusi_book.dart';
 import 'package:readme/modules/home-page/models/book.dart';
 
 class DetailsPage extends StatefulWidget {
